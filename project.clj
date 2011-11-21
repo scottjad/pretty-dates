8fa398674dbb67119506082b494d3cc6b64894d5
Scott Jaderholm <jaderholm@gmail.com>
2011-11-21 15:50:45 -0500

    add url to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject pretty-dates "0.1.0-SNAPSHOT"
   :description "Pretty dates (also called human readable). Ex: 2 hours ago."
+  :url "http://github.com/scottjad/pretty-dates"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-time "0.3.3"]])
